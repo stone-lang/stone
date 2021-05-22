1bb4eea8df4c2faa33e11a9fe0d527918092bbbb
Craig Buchek <craig@boochtek.com>
2021-05-22 10:21:14 -0700

    Clean up grammar for literals

--- src/stone/language/literals.rb
+++ src/stone/language/literals.rb
@@ -5,21 +5,30 @@ module Stone
   module Language
     class Literals < Base
 
-      DECIMAL_EXPONENT_SYMBOL = "\u23E8"
+      DECIMAL_EXPONENT_SYMBOL = "\u23E8" # NOTE: I'm not 100% sure this is an appropriate use of this symbol.
 
       def grammar
         Class.new(super) do |_klass|
           override rule(:expression) { literal | parenthetical_expression }
-          rule(:literal) { decimal | rational | integer | string }
+          rule(:literal) { decimal | rational | integer | text }
           rule!(:integer) { binary_integer | octal_integer | hexadecimal_integer | decimal_integer }
           rule!(:decimal) { decimal_integer >> str(".") >> unsigned_decimal_integer >> (match["eE#{DECIMAL_EXPONENT_SYMBOL}"] >> decimal_integer).maybe }
           rule!(:rational) { decimal_integer.as(:numerator) >> str("/") >> unsigned_decimal_integer.as(:denominator) }
-          rule!(:string) { str('"').ignore >> (str('"').absent? >> any).repeat(0) >> str('"').ignore }
-          rule(:decimal_integer) { match["+-"].maybe >> unsigned_decimal_integer }
-          rule(:unsigned_decimal_integer) { match["0-9_"].repeat(1) }
-          rule(:hexadecimal_integer) { match["+-"].maybe >> str("0x") >> match["[:xdigit:]"] >> match["[:xdigit:]_"].repeat(0) }
-          rule(:octal_integer) { match["+-"].maybe >> str("0o") >> match["0-7"] >> match["0-7_"].repeat(0) }
-          rule(:binary_integer) { match["+-"].maybe >> str("0b") >> match["0-1"] >> match["0-1_"].repeat(0) }
+          rule!(:text) { str('"').ignore >> (str('"').absent? >> any).repeat(0) >> str('"').ignore }
+          rule(:decimal_integer) { sign.maybe >> unsigned_decimal_integer }
+          rule(:unsigned_decimal_integer) { digit >> digit_.repeat(0) }
+          rule(:hexadecimal_integer) { sign.maybe >> str("0x") >> xdigit >> xdigit_.repeat(0) }
+          rule(:octal_integer) { sign.maybe >> str("0o") >> odigit >> odigit_.repeat(0) }
+          rule(:binary_integer) { sign.maybe >> str("0b") >> bdigit >> bdigit_.repeat(0) }
+          rule(:sign) { match["+-"] }
+          rule(:digit) { match["[:digit:]"] }
+          rule(:xdigit) { match["[:xdigit:]"] }
+          rule(:odigit) { match["[0-7]"] }
+          rule(:bdigit) { match["[0-1]"] }
+          rule(:digit_) { digit | str("_") }
+          rule(:xdigit_) { xdigit | str("_") }
+          rule(:odigit_) { odigit | str("_") }
+          rule(:bdigit_) { bdigit | str("_") }
         end
       end
 
@@ -40,7 +49,7 @@ module Stone
               Stone::AST::Rational.new(numerator, denominator)
             end
           }
-          rule(string: simple(:t)) {
+          rule(text: simple(:t)) {
             Stone::AST::Text.new(t)
           }
 
